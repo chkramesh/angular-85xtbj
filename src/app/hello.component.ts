f81e351c7aa4e32fa71a46033f10bf8ae9580f1e
chkramesh <chkramesh@gmail.com>
2019-04-30 15:04:16 -0400

    4 files modified

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -2,8 +2,10 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'hello',
-  template: `<h1>Hello {{name}}!</h1>`,
-  styles: [`h1 { font-family: Lato; }`]
+  template: `<mat-toolbar color="primary">
+              <h1><span class="heart">❤</span> {{ name }} </h1>
+            </mat-toolbar>`,
+  styles: [`h1 { font-family: Lato; } .heart { color: #f95372; }`]
 })
 export class HelloComponent  {
   @Input() name: string;
